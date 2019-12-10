bfe573be0caf0aff7ad872a3d052ccd47005c8d2
Mohamed Abdelwahab <m.taha2000@outlook.de>
2019-12-10 21:12:23 +0100

    Added a bash code snippet for configuring sshd_config

--- Bash/CodeSnippets.sh
+++ Bash/CodeSnippets.sh
@@ -1,2 +1,5 @@
 # cd to script path
 cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1
+
+# Programmatically change PasswordAuthentication to yes in sshd_config
+sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config

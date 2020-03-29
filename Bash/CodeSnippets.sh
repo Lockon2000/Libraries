4589d2a326592871cd4eaeedeb24f1f49a966fb4
Mohamed Abdelwahab <m.taha2000@outlook.de>
2020-03-30 01:54:25 +0200

    Add a bash code snippet for programmatically getting the fingerprint

--- Bash/CodeSnippets.sh
+++ Bash/CodeSnippets.sh
@@ -3,3 +3,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1
 
 # Programmatically change PasswordAuthentication to yes in sshd_config
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+
+# Programmatically add the fingerprint of a host to ~/.ssh/known_hosts
+ssh-keyscan -H "<host>" >> ~/.ssh/known_hosts
+
